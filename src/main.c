1a8dcfecb74829a873342588db4a8a80fe68567d
Sqyd <wojkap10@proton.me>
2025-06-25 14:12:22 +0200

    Made sceleton

--- src/main.c
+++ src/main.c
@@ -1,6 +1,17 @@
 #include <raylib.h>
 
 int main() {
+	InitWindow(640, 640, "Snakey");
+	SetTargetFPS(60);
+
+	while(!WindowShouldClose()) {
+		BeginDrawing();
+		ClearBackground(WHITE);
+
+		EndDrawing();
+	}
+
+	CloseWindow();
 
 	return 0;
 }
